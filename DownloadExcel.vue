4f422215da760811d9b50d365e4956dfeb5ad94e
Hayk <hayk.balasanyan1@gmail.com>
2019-08-01 12:56:35 +0400

    added functional for download xlsx file

---
+++ DownloadExcel.vue
@@ -0,0 +1,61 @@
+<template>
+    <button @click="exportExcel">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    import XLSX from 'xlsx/xlsx';
+    window.$ = window.jQuery = require('jquery');
+
+    export default {
+        name: "download-excel",
+
+        props: {
+            columns: {
+                type: Array,
+                default: []
+            },
+            data: {
+                type: Array,
+                default: []
+            },
+            filename: {
+                type: String,
+                default: 'excel'
+            }
+        },
+
+        methods: {
+            exportExcel() {
+                let createXLSLFormatObj = [];
+                let newXlsHeader = [];
+                $.each(this.columns, function(index, value) {
+                    newXlsHeader.push(value.label);
+                });
+
+                createXLSLFormatObj.push(newXlsHeader);
+                $.each(this.data, function(index, value) {
+                    let innerRowData = [];
+                    $.each(this.columns, function(index, val) {
+                        innerRowData.push(value[val.field]);
+                    });
+                    createXLSLFormatObj.push(innerRowData);
+                });
+                /* File Name */
+                let filename = this.filename + ".xlsx";
+                //
+                // /* Sheet Name */
+                let ws_name = "FreakySheet";
+                //
+                if (typeof console !== 'undefined') console.log(new Date());
+                let wb = XLSX.utils.book_new(),
+                    ws = XLSX.utils.aoa_to_sheet(createXLSLFormatObj);
+
+                // /* Add worksheet to workbook */
+                XLSX.utils.book_append_sheet(wb, ws, ws_name);
+                XLSX.writeFile(wb, filename);
+            }
+        }
+    }
+</script>
---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-excel-xlsx
+
+Package development is not complete. Please don't instal!
